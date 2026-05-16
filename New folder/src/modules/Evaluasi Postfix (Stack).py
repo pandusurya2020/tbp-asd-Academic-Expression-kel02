f5f134c4493a696131332faa6c34ccdf4e43f582
gifarahanifa-h <gifara6511ft.2025@student.uny.ac.id>
2026-05-16 17:34:20 +0700

    feat(shunting-yard): implement stack and core expression modules

--- New folder/src/modules/Evaluasi Postfix (Stack).py
+++ New folder/src/modules/Evaluasi Postfix (Stack).py
@@ -1,21 +1,26 @@
 import sys
 import os
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from data_structures.stack import Stack
 
 def eval_postfix(postfix_tokens: list, var_bst=None) -> float:
-    eval_stack = Stack()  # Stack untuk menampung angka perhitungan
+    """Menghitung hasil akhir dari rumus postfix menggunakan Stack."""
+    eval_stack = Stack()
 
     for tok in postfix_tokens:
-        # Jika token berupa angka
+        # Jika token adalah angka
         if tok.replace('.', '', 1).isdigit():
             eval_stack.push(float(tok))
-        # Jika token berupa operator (+, -, *, /)
-        elif tok in ['+', '-', '*', '/']:
+        # Jika token adalah operator, lakukan perhitungan
+        elif tok in ['+', '-', '*', '/', '^']:
             b = eval_stack.pop()
             a = eval_stack.pop()
             
+            if a is None or b is None:
+                raise SyntaxError("Rumus tidak valid")
+
             if tok == '+':
                 eval_stack.push(a + b)
             elif tok == '-':
@@ -23,6 +28,10 @@ def eval_postfix(postfix_tokens: list, var_bst=None) -> float:
             elif tok == '*':
                 eval_stack.push(a * b)
             elif tok == '/':
+                if b == 0:
+                    raise ZeroDivisionError("Tidak bisa membagi dengan angka nol")
                 eval_stack.push(a / b)
+            elif tok == '^':
+                eval_stack.push(a ** b)
 
-    return eval_stack.pop()  # Hasil akhir berada di tumpukan paling atas
+    return eval_stack.pop()
