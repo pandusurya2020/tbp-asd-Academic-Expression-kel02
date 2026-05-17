1ed0c163a6531cdd97175edb1512602c5ebddb3b
gifarahanifa-h <gifara6511ft.2025@student.uny.ac.id>
2026-05-18 03:02:33 +0700

    fix expression tree dan fix test linked list

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,14 +1,12 @@
-from src.data_structures.linked_list import LLNode
-
+from src.data_structures.linked_list import Node
 
 def test_linked_list():
-
     print("=== TEST LINKED LIST NODE ===")
 
-    # Membuat node
-    node1 = LLNode(10)
-    node2 = LLNode(20)
-    node3 = LLNode(30)
+    # Membuat node menggunakan kelas Node yang benar
+    node1 = Node(10)
+    node2 = Node(20)
+    node3 = Node(30)
 
     # Menyambungkan node
     node1.next = node2
@@ -20,7 +18,6 @@ def test_linked_list():
     while current:
         print(current.data)
         current = current.next
-
 
 # Jalankan test
 test_linked_list()
